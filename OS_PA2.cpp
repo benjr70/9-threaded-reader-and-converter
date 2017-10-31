0f965d93c97ad62a32d1969d718eff22a61bdbae
Benjr70 <benrolf70@gmail.com>
2017-10-31 12:26:40 -0700

    thread set up

--- OS_PA2.cpp
+++ OS_PA2.cpp
@@ -5,10 +5,64 @@
 
 using namespace std;
 
+//prototypes
+void *reader(void *arg);
+void *converter(void *arg);
+void *writer(void *arg);
+
+//global vars
+int NUM_THREADS = 3;
+
+
 int main(){
+	pthread_t Rid[NUM_THREADS];
+	pthread_t Cid[NUM_THREADS];
+	pthread_t Wid[NUM_THREADS];
 
-cout << "hello world\n";
+	for(int i = 0; i < NUM_THREADS; i++){
+		pthread_create(&Rid[i], NULL, reader,NULL);
+		//cout << Rid[i] <<"\n";
+		pthread_create(&Cid[i], NULL, converter,NULL);
+		//cout << Cid[i] <<"\n";
+		pthread_create(&Wid[i], NULL, writer,NULL);
+		//cout << Wid[i] <<"\n";
+	}
 
+	for(int i = 0; i < NUM_THREADS; i++){
+		pthread_join(Rid[i], NULL);
+		pthread_join(Cid[i], NULL);
+		pthread_join(Wid[i], NULL);
+	}
 
-return 0;
+	return 0;
+}
+
+//**********************
+//
+//
+//**********************
+void *reader(void *arg){
+
+	cout << "reader\n";
+
+}
+
+//**********************
+//
+//
+//**********************
+void *converter(void *arg){
+
+	cout << "converter\n";
+
+}
+
+//**********************
+//
+//
+//**********************
+void *writer(void *arg){
+
+	cout << "writer\n";
+
 }
